992ff684789da552c6952e42e19d9797408edb68
DharsDev <dharshanpalani.2006@gmail.com>
2024-09-24 14:47:27 +0530

    Project set up finished

---
+++ src/topBar.jsx
@@ -0,0 +1,17 @@
+import './css/topbar.css'
+
+function Topbar() {
+ 
+  return(
+    <> 
+      <div className="topbar">
+
+    <h1> Top bar</h1>
+
+    </div>
+    </>
+  )
+}
+
+
+export default Topbar
